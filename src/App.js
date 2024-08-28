ce83a2f45619b0d1670da3658f4630696de71746
M1NGJU <s2303@e-mirim.hs.kr>
2024-08-28 15:25:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { Container } from '@mui/material';
 import './App.css';
+// import Main from './Main';
 
-function App() {
+import Event from './event'; // 파일 경로와 컴포넌트 이름 수정
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {/* <Main /> */}
+      <Event /> {/* 컴포넌트 이름 수정 */} 
+    </Container>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react"; // React import
+import './Main.css'; // Main.css 파일을 import
+import { Button, TextField } from '@mui/material'; // Material-UI Button, TextField import
+
+function Grop() {
+    return (
+        <div className="Card1">
+            <div className="c1image">
+                <img className="phoneImage" alt="Group" src="img/Group.png" />
+            </div>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <h1 className="large-text">
+                우리 팀 경기를<br />
+                알아서 챙겨주는,
+            </h1>
+            <Grop /> {/* Grop 컴포넌트를 호출하여 렌더링 */}
+            <Login /> {/* Login 컴포넌트를 호출하여 렌더링 */}
+        </div>
+    );
+}
+
+function Login() {
+    const [username, setUsername] = useState(""); // 사용자 이름 상태
+    const [password, setPassword] = useState(""); // 비밀번호 상태
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // 기본 폼 제출 방지
+        // 로그인 처리 로직 추가
+        console.log("로그인 시도:", { username, password });
+    };
+
+    return (
+        <div className="login-container"> {/* 가운데 정렬을 위한 클래스 추가 */}
+            <form onSubmit={handleSubmit} className="login-form">
+                <TextField 
+                    label="아이디를 입력해주세요" 
+                    variant="outlined" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
+                    fullWidth 
+                    margin="dense" // 마진을 "dense"로 설정하여 간격을 좁힘
+                />
+                <TextField 
+                    label="비밀번호를 입력해주세요" 
+                    type="password" 
+                    variant="outlined" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    fullWidth 
+                    margin="dense" // 마진을 "dense"로 설정하여 간격을 좁힘
+                />
+                <Button 
+                    variant="contained" 
+                    className="login-button" // CSS 클래스 추가
+                    type="submit"
+                >
+                    확인
+                </Button>
+            </form>
+            <Text /> {/* Text 컴포넌트를 호출하여 렌더링 */}
+            <Login2 /> {/* 간편 로그인 컴포넌트 추가 */}
+            <LoginPic /> {/* LoginPic 컴포넌트를 호출하여 렌더링 */}
+        </div>
+    );
+}
+
+function Text() {
+    return (
+        <div className="text1">
+            <h5>아이디 찾기 | 비밀번호 찾기 | 회원가입</h5>
+        </div>
+    );
+}
+
+function Login2() {
+    return (
+        <div className="login2">
+            <h5>간편 로그인</h5>
+        </div>
+    );
+}
+
+function LoginPic() {
+    return (
+        <div className="loginPic">
+            <div className="google">
+                <img className="googleImg" alt="Google 로그인" src="img/google.png" />
+            </div>
+            <div className="kakao">
+                <img className="kakaoImg" alt="kakao 로그인" src="img/kakao.png" />
+            </div>
+            <div className="naver">
+                <img className="naverImg" alt="naver 로그인" src="img/naver.png" />
+            </div>
+
+        </div>
+    );
+}
+
+export default App; // App 컴포넌트를 기본 내보내기로 설정
+export { Grop }; // Grop 컴포넌트를 명명된 내보내기로 설정
+export { Login }; // Login 컴포넌트를 명명된 내보내기로 설정
+export { Text }; // Text 컴포넌트를 명명된 내보내기로 설정
+export { Login2 }; // 간편 로그인 컴포넌트를 명명된 내보내기로 설정
+export { LoginPic }; // LoginPic 컴포넌트를 명명된 내보내기로 설정
