7639adfd4ab04b73ca7f68d66c55a83dd8ea5fc4
M1NGJU <s2303@e-mirim.hs.kr>
2024-08-28 15:32:35 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Container } from '@mui/material';
 import './App.css';
-// import Main from './Main';
+import Main from './Main';
 
-import Event from './event'; // 파일 경로와 컴포넌트 이름 수정
 
 const App = () => {
   return (
     <Container>
-      {/* <Main /> */}
-      <Event /> {/* 컴포넌트 이름 수정 */} 
+      <Main />
     </Container>
   );
 }
